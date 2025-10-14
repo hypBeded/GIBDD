d3c1196d04bd2918d77f61aa4e88dfa0ca043202
HypBed <uymaevymat@gmail.com>
2025-10-14 14:49:15 +0300

    Начал делать добавление изображения

--- Windows/DriverADD.xaml.cs
+++ Windows/DriverADD.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Windows
 {
@@ -59,13 +60,13 @@ namespace Windows
             string FN = FullName.Text; //ФИО
             string PI = PasportInfo.Text; //Паспортные данные
             string PN = PhoneNumber.Text; // Номер телефона
-            string mail = Email.Text ; // почта
-            string remark = Zamechanie.Text ;// замечание
+            string mail = Email.Text; // почта
+            string remark = Zamechanie.Text;// замечание
             string RegA = RegAdress.Text; //адрес регистр
             string ResA = ResAdress.Text; //адрес проживание
             string POW = PlaceOfWork.Text; //Место работы
             string post = Post.Text; //Должность
-            byte image;
+            byte? image = null;
 
 
 
@@ -79,57 +80,56 @@ namespace Windows
             using (var conect = new SqliteConnection("Data Source=GIBDD.db"))
             {
                 conect.Open();
-                SqliteCommand cmd = new SqliteCommand();
-                cmd.Connection = conect;
-                SqliteCommand sqliteCommand = new SqliteCommand();
+                var command = conect.CreateCommand();
+                command.CommandText = "Insert INTO Drivers" +
+                    "(Full_Name, Pasporta_data,Phone_number,Email,Remark,Adres_regitr,Adres_residinatia,Workplace,Post,Photo) " +
+                    "values  (@Full_Name,@Pasporta_data,@Phone_number,@Email,@Remark,@Adres_regitr,@Adres_residinatia,@Workplace,@Post,@Photo";
 
-                
+                command.Parameters.AddWithValue("@Full_Name", FN );
+                command.Parameters.AddWithValue("@Pasporta_data",PI );
+                command.Parameters.AddWithValue("@Phone_number",PN );
+                command.Parameters.AddWithValue("@Email",mail );
+                command.Parameters.AddWithValue("@Remark", remark);
+                command.Parameters.AddWithValue("@Adres_regitr",RegA );
+                command.Parameters.AddWithValue("@Adres_residinatia",ResA);
+                command.Parameters.AddWithValue("@Workplace",POW );
+                command.Parameters.AddWithValue("@Post", post);
+                command.Parameters.AddWithValue("@Photo", image);
 
-               
-                   
-
-
-
-                
+                command.ExecuteNonQuery();
             }
-
         }
-        public class User
-        {
-            public int id { get; set; }
-            public string FullName { get; set; }
-            public string email { get; set; }
-
-            public int PasportInfo { get; set; }
-            public int PhoneNubmer { get; set; }
-            public byte[] Image { get; set; }
-            public string Zamechania { get; set; }
-            public string RegAddress { get; set; }
-            public string ResidentialAddress { get; set; }
-            public string PlaceOfWork { get; set; }
-            public string post { get; set; }
 
 
-        
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+
        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         private void DriverImage_Add(object sender, MouseButtonEventArgs e)
         {
+            
+           /* var openFileDialog = new Microsoft.Win32.OpenFileDialog
+            {
+                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+            };
 
+            if (openFileDialog.ShowDialog() == true)
+            {
+                byte imageByte;
+                // Преобразуем изображение в массив байтов
+                imageByte = File.ReadAllBytes(openFileDialog.FileName);
+                MessageBox.Show("Изображение загружено!");
+            } */
         }
     }
 }
