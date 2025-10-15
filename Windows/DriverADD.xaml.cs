a90fcb9e2860604bd94d28fe94252eba6372a0bb
HypBed <uymaevymat@gmail.com>
2025-10-15 19:09:45 +0300

    Код для окна регистрации водителей готов

--- Windows/DriverADD.xaml.cs
+++ Windows/DriverADD.xaml.cs
@@ -28,90 +28,45 @@ namespace Windows
         public DriverADD()
         {
             InitializeComponent();
-
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        byte[] imageBytes;
-
-
+        
+        byte[] imageBytes; //Временное хранение изображения в байтах
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             string FN = FullName.Text; //ФИО
+            int PN = Convert.ToInt32(PhoneNumber.Text); //Номер телефона
 
-            if (PasportInfo.Text != null)
-            {
-                Regex regex = new Regex(@"^\d{11}$");
+            //Паспортные данные-----------------------------------------------------------
+            string Pi = $"{PasportInfo.Text}{PasportInfo1.Text} "; //Объединение паспортных данных в одну сплошную строку
+            int PI = Convert.ToInt32(Pi);
 
-                if (!regex.IsMatch(PasportInfo.Text))
-                {
-                    MessageBox.Show("Неверный формат. Используйте  11 цифр.");
-                    return;
-                }
-            }
-            int PI = Convert.ToInt32(PasportInfo.Text); //Паспортные данные
-                                                        //
-            if (PhoneNumber.Text != null)
-            {
-                // Паттерн: 11 цифр
-                Regex regex = new Regex(@"^\d{11}$");
-                if (!regex.IsMatch(PhoneNumber.Text))
-                {
-                    MessageBox.Show("Неверный формат номера телефона. Используйте 11 цифр.");
-                    return;
-                }
-            }
-            int PN = Convert.ToInt32(PhoneNumber.Text); //номер телефона
-                                                        //
-
-            if (!ValidateEmail(Email.Text))
+            if (!ValidateEmail(Email.Text)) //Валидация почты
             {
                 MessageBox.Show("Некорректный email.", "Ошибка");
                 return;
             }
+            //--------------------------------------------------------------------------------
             string mail = Email.Text; // почта
             string remark = Zamechanie.Text;// замечание
-            //
+                                            //--------------------------------------------------------------------------------
             string city1 = City1.Text;
             string city2 = City2.Text;
-            //
             string adress1 = Adress1.Text;
             string adress2 = Adress2.Text;
-            //
-            string RegA = ($"{city1}, {adress1}");    //адрес регистр
+            //--------------------------------------------------------------------------------
+            string RegA = ($"{city1}, {adress1}"); //адрес регистр
             string ResA = ($"{city2}, {adress2}"); //адрес проживание
             string POW = PlaceOfWork.Text; //Место работы
             string post = Post.Text; //Должность
-            byte[] image = imageBytes;
-            //
+            byte[] image = imageBytes; //Изображение в байтах
+            //--------------------------------------------------------------------------------
             using (var conect = new SqliteConnection("Data Source=GIBDD.db"))
             {
                 try
                 {
                     conect.Open();
                     var command = conect.CreateCommand();
-                    command.CommandText = "Insert INTO Drivers" +
+                    command.CommandText = "Insert INTO Drivers" + 
                         "(Full_Name, Pasporta_data,Phone_number,Email,Remark,Adres_regitr,Adres_residinatial,Workplace,Post,Photo) " +
                         "values  (@Full_Name,@Pasporta_data,@Phone_number,@Email,@Remark,@Adres_regitr,@Adres_residinatial,@Workplace,@Post,@Photo)";
 
@@ -129,63 +84,43 @@ namespace Windows
                     command.ExecuteNonQuery();
                     MessageBox.Show("Запись добавлена");
                 }
-                catch (Exception ex)
+                catch (Exception ex) //Обработка исключений
                 {
                     MessageBox.Show("Убедитесь что вы заполнили все " + ex.Message);
                     return;
                 }
             }
-
+            //Сброс всех полей после доабвления в таблицу------------------
             FullName.Text = string.Empty; //ФИО
-            PasportInfo.Text = string.Empty;  //"серия и номер без пробела";  //Паспортные данные
-            PhoneNumber.Text = string.Empty; //"Номер телефона без символов";  // Номер телефона
+            PasportInfo.Text = string.Empty; //Паспортные данные
+            PhoneNumber.Text = string.Empty; // Номер телефона
             Email.Text = string.Empty; ; // почта
             Zamechanie.Text = string.Empty; // замечание
-            //                                
-            City1.Text = string.Empty;
-            City2.Text = string.Empty;
-            //
-            Adress1.Text = string.Empty;
-            Adress2.Text = string.Empty;
-            //
-
             PlaceOfWork.Text = string.Empty; ; //Место работы
             Post.Text = string.Empty; ; //Должность
-
+                                        //Города-------------------------------------------------------                            
+            City1.Text = string.Empty;
+            City2.Text = string.Empty;
+            //Адреса-------------------------------------------------------
+            Adress1.Text = string.Empty;
+            Adress2.Text = string.Empty;
+            //Сброс фотографии---------------------------------------------
             string filePath = "/Image2.png";
             var bitmap = new BitmapImage(new Uri(filePath, UriKind.Relative));
             DriverImage.Source = bitmap;
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         private void DriverImage_Add(object sender, MouseButtonEventArgs e)
-        {
-
-            // Создаем экземпляр OpenFileDialog
+        { 
             OpenFileDialog openFileDialog = new OpenFileDialog
             {
-                Filter = "Image Files|*.jpg;*.png;" // Фильтр для изображений
+                Filter = "Image Files|*.jpg;*.png;" // Фильтр .jpg и .png
             };
 
             if (openFileDialog.ShowDialog() == true)
             {
-                string filePath = openFileDialog.FileName;
-                imageBytes = File.ReadAllBytes(filePath);
+                string filePath = openFileDialog.FileName; //Получение пути к изображению
+                imageBytes = File.ReadAllBytes(filePath); //Преобразование изображения в байты
 
                 using (var stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
                 {
@@ -197,57 +132,64 @@ namespace Windows
                     bitmap.Freeze(); // Замораживаем изображение для использования в UI
                     DriverImage.Source = bitmap; // Заменяем текущее изображение
                 }
+            }
+        }
+        private void PasportInfo_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text) //Проверка на цифры
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
 
-                MessageBox.Show("Изображение загружено!");
+            TextBox textBox = (TextBox)sender;  //Проверка на 4 символа
+            string newText = textBox.Text + e.Text;
+            if (newText.Length > 4)
+            {
+                e.Handled = true;
+            }
+        }
+        private void PasportInfo1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)//Проверка на цифры
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
+
+            TextBox textBox = (TextBox)sender; //Проверка на 6 символов
+            string newText = textBox.Text + e.Text;
+            if (newText.Length > 6)
+            {
+                e.Handled = true;
+            }
+        }
+        private void PhoneNumber_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            foreach (char c in e.Text)//Проверка на цифры
+            {
+                if (!char.IsDigit(c))
+                {
+                    e.Handled = true;
+                    return;
+                }
+            }
+
+            TextBox textBox = (TextBox)sender; //Проверка на 11 символов
+            string newText = textBox.Text + e.Text;
+            if (newText.Length > 11)
+            {
+                e.Handled = true;
             }
         }
 
-
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void PasportInfo_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            //        var textBox = (TextBox)sender;
-            //if (textBox != null)
-            //{
-            //string currentText = textBox.Text;
-
-            //// Получаем новый текст после вставки
-            //string newText = currentText.Insert(textBox.SelectionStart, e.Text);
-
-            //// Проверяем, соответствует ли новый текст формату: ровно 11 цифр
-            //Regex regex = new Regex(@"^\d{11}$");
-
-            //// Если новый текст не соответствует паттерну, отменяем ввод
-            //e.Handled = !regex.IsMatch(newText);
-        }
-    
-
-        private void PhoneNumber_PreviewTextInput(object sender, TextCompositionEventArgs e)
-        {
-            //var textBox = (TextBox)sender;
-            //if (textBox != null)
-            //{
-            //    string currentText = textBox.Text;
-
-            //    // Получаем новый текст после вставки
-            //    string newText = currentText.Insert(textBox.SelectionStart, e.Text);
-
-            //    // Проверяем, соответствует ли новый текст формату: ровно 11 цифр
-            //    Regex regex = new Regex(@"^\d{11}$");
-
-            //    // Если новый текст не соответствует паттерну, отменяем ввод
-            //    e.Handled = !regex.IsMatch(newText);
-            //}
-        }
-
-
-
-        private readonly string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-
+        private readonly string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"; //паттерн для проверки почты
         public bool ValidateEmail(string email)
         {
             if (string.IsNullOrEmpty(email))
@@ -255,40 +197,7 @@ namespace Windows
                 return false;
             }
             return Regex.IsMatch(email, emailPattern);
-        }
-
-        private void PasportInfo_GotFocus(object sender, RoutedEventArgs e)
-        {
-            //if (PasportInfo.Text == "серия и номер без пробела")
-            //{
-            //    PasportInfo.Text = string.Empty;
-            //}
-
-        }
-
-        private void PasportInfo_LostFocus(object sender, RoutedEventArgs e)
-        {
-            //if (string.IsNullOrEmpty(PasportInfo.Text)) {
-            //    PasportInfo.Text = "серия и номер без пробела";
-            //}
-        }
-
-        private void PhoneNumber_GotFocus(object sender, RoutedEventArgs e)
-        {
-            //if (PhoneNumber.Text == "Номер телефона без символов")
-            //{
-            //    PhoneNumber.Text = string.Empty;
-            //}
-        }
-
-        private void PhoneNumber_LostFocus(object sender, RoutedEventArgs e)
-        {
-            //if (string.IsNullOrEmpty(PhoneNumber.Text))
-            //{
-            //    PhoneNumber.Text = "Номер телефона без символов";
-            //}
-        }
-    }
-    
-    }
+        } //метод для проверки почты
+    }  
+}
 
