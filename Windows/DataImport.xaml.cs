b7a4a7d09a763192428dd398bb2e5e6a7366b064
Matvei <matveiabramov2007@gmail.com>
2025-10-27 20:39:40 +0300

    начал работать с json и добавил в бд таблицу licence(вроде так)

--- Windows/DataImport.xaml.cs
+++ Windows/DataImport.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Data.Sqlite;
+using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +22,7 @@ namespace Windows
     /// </summary>
     public partial class DataImport : Window
     {
+        private string filesPath;
         public DataImport()
         {
             InitializeComponent();
@@ -27,17 +30,39 @@ namespace Windows
 
         private void Add_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON Files|*.json;" // Фильтр .json
+            };
+            openFileDialog.ShowDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                filesPath = openFileDialog.FileName;
+            }
             using (var conect = new SqliteConnection("Data Source=GIBDD.db"))
             {
                 conect.Open();
-                SqliteCommand cmd = new SqliteCommand();
-                cmd.Connection = conect;
-                cmd.CommandText = "CREATE TABLE Driver_licence(" +
-                    "";
-                //cmd.CommandText = "CREATE TABLE Penalties(_id INTEGER NOT NULL PRIMARY KEY UNIQUE, postNum INTEGER NOT NULL, postDate, TEXT NOT NULL, suma INTEGER NOT NULL, koapCode TEXT NOT NULL, koapText TEXT NOT NULL, address TEXT NOT NULL, license_plate_number , paid INTEGER NOT NULL CHECK(paid IN (0,1))";
-                cmd.ExecuteNonQuery();
 
-            }
+            //    SqliteCommand cmd = new SqliteCommand();
+            //    cmd.Connection = conect;
+
+            //    //cmd.CommandText = "CREATE TABLE Penalties(_id INTEGER NOT NULL PRIMARY KEY UNIQUE, postNum INTEGER NOT NULL, postDate, TEXT NOT NULL, suma INTEGER NOT NULL, koapCode TEXT NOT NULL, koapText TEXT NOT NULL, address TEXT NOT NULL, license_plate_number , paid INTEGER NOT NULL CHECK(paid IN (0,1))";
+            //    cmd.ExecuteNonQuery();
+
+                }
+        }
+        private class fine
+        {
+            public int id {  get; set; }
+            public int postNum { get; set; }
+            public string postDate { get; set; }
+            public int sum { get; set; }
+            public int koapCode { get; set; }
+            public string koapText { get; set; }
+            public string address { get; set; }
+            public string dataCar { get; set; }
+            public int dataDriver { get; set; }
+
         }
     }
 }
